f931e9f37fd0cbfcf10e25c6d64adc71e1469487
Maxim Frolenko <maximzec3200@gmail.com>
2020-10-01 17:09:32 +0300

    add orders routes

--- api/orders.js
+++ api/orders.js
@@ -1,7 +1,5 @@
 const express = require('express');
-const router = express.Router();
-
-
+const router = express.Router({mergeParams: true});
 
 router.get('/' , (req, res, next) => {
     res.status(200).json({
@@ -16,7 +14,7 @@ router.post('/', (req, res, next) => {
     });
 });
 
-router.get('/:restID', (req, res, next) => {
+router.get('/:orderID', (req, res, next) => {
     const id = req.params.restID;
     if (id === 'special') {
         res.status(200).json({
@@ -25,7 +23,7 @@ router.get('/:restID', (req, res, next) => {
         });
     } else {
         res.status(200).json({
-            message: 'SUCCEFULY PASSED ID1',
+            message: 'SUCCEFULY PASSED ID12',
             id: id
         });
     }
